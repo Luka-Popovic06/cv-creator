994d42ed49ea609ae12aa9271914cd17ec99e154
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-07 17:31:35 +0100

    add reusable Input component

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,1 +1,13 @@
-
+const Input = ({ inputType, inputName, variation }) => {
+  let inputClass;
+  if (variation === "standard") inputClass = "standarad-input";
+  return (
+    <input
+      type={inputType}
+      name={inputName}
+      placeholder={inputName}
+      className={inputClass}
+    />
+  );
+};
+export default Input;
