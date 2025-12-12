f2e00cae9b2de3e80281df86632137122b83aefd
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-12 20:29:34 +0100

    Update Button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,1 +1,17 @@
-
+const Button = ({ variation, children, btnType, clickAction }) => {
+  const baseClass = "btn";
+  let btnClass;
+  if (variation === "standard") btnClass = "btn-standard";
+  if (variation === "primary") btnClass = "btn-primary";
+  if (variation === "secondary") btnClass = "btn-secondary";
+  return (
+    <button
+      type={btnType}
+      className={`${baseClass} ${btnClass}`}
+      onClick={clickAction}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
